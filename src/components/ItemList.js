a84b779c0365410c7329f7d0f487490741cb6579
benetta <ksenyatokareva@gmail.com>
2019-08-11 12:35:38 +0300

    Add ProductItem, fix layout

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import ProductItem from './ProductItem'
+
+const ratingComponent = () => {
+  return <div className="starFill"/>;
+};
 
 function ItemList({ data }) {
-	let items = data.map(item => 
-		<li key={item.id}>{item.name}</li> 
-	);
+
+	let items = data.map(item =>{
+		let data = JSON.parse(item)
+				
+		return <ProductItem 
+			key={data.title}
+			isInStock={data.isInStock}
+			img={data.img}
+			title={data.title}
+			price={data.price}
+			subPriceContent=" ₽"
+			maxRating={data.maxRating}
+			rating={data.rating}
+			ratingComponent={ratingComponent} />
+		});
 	
-	return <ul className='item-list'>{items}</ul>
+	return <div className='item-list'>{items}</div>
 }
 
 export default ItemList
